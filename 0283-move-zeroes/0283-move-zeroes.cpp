9c06eb5fef465880270952b0b279711b87507b54
Anant Pandey <114588705+anant-365@users.noreply.github.com>
2024-10-24 22:36:55 +0530

    Time: 0 ms (100.00%), Space: 21.9 MB (25.82%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonzeroindex = 0;
+        for(int i=0; i<nums.size(); i++){
+               if(nums[i]!=0){
+                   nums[nonzeroindex++] = nums[i];
+               }
+        }
+        for(int i=nonzeroindex; i<nums.size(); i++){
+            nums[i]=0;
+        }
+        
+    }
+};
