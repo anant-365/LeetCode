0e500dd0cdfc93b1b9c3b0f49b01bd451d542191
Anant Pandey <114588705+anant-365@users.noreply.github.com>
2024-10-26 20:10:00 +0530

    Time: 15 ms (50.90%), Space: 16.5 MB (7.94%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       map<int, int> hashtable;
+        int rem = 0;
+        vector<int> arr;
+        for(int i=0; i<nums.size(); i++){
+            rem = target-nums[i];
+            if(hashtable.find(rem)!=hashtable.end()){
+                arr.push_back(hashtable[rem]);
+                arr.push_back(i);
+            }
+            hashtable[nums[i]] = i;
+        }
+        return arr;
+    }
+};
