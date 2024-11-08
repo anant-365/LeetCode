5da29fe079b6da5b045ef21575ae9789e367f2b0
Anant Pandey <114588705+anant-365@users.noreply.github.com>
2024-11-08 16:50:05 +0530

    Time: 3 ms (45.98%), Space: 8.7 MB (81.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -5,7 +5,7 @@ public:
         int i = 0, j = 0;
         s.erase(0, s.find_first_not_of(" \t\n\r\f\v"));
         s.erase(s.find_last_not_of(" \t\n\r\f\v") + 1);
-        s += ' ';
+        s += ' ';   
         while (j <= n) {
             if (s[j] == ' ' || j == n) {
                 reverse(s.begin() + i, s.begin() + j);
@@ -15,7 +15,14 @@ public:
         }
         s.erase(s.find_last_not_of(" \t\n\r\f\v") + 1);
         reverse(s.begin(), s.end());
-        regex multiple_spaces("\\s+");
-        return regex_replace(s, multiple_spaces, " ");
+        int slow = 0;  
+        for (int fast = 0; fast < s.size(); fast++) {
+            if (s[fast] != ' ' || (fast > 0 && s[fast - 1] != ' ')) {
+                s[slow++] = s[fast];  
+            }
+        }
+        s.resize(slow);
+        
+        return s;
     }
 };
